cb8917b870985cece10a98909d374b77d7348537
Pablo Javier Alemán Velásquez <paleman-2019222@kinal.edu.gt>
2021-07-29 21:55:47 -0600

    cambio en inscription

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -410,9 +410,6 @@ function getTeachers(req, res){
 function inscription (req,res){
     var studentId = req.params.idS;
     var classId = req.params.idC;
-    if(studentId != req.user.sub){
-        res.status(403).send({message:'No puede acceder a esta función'})
-    }else{
         Class.findOne(({student:studentId, _id:classId}), (err, userFind)=>{
             if(err){
                 return res.status(500).send({message: 'ERROR GENERAL', err});
@@ -439,7 +436,7 @@ function inscription (req,res){
     }
         })
     
-}
+
 }
 /*DELETE USER BY ADMIN*/
 function deleteUserByAdmin(req, res){
